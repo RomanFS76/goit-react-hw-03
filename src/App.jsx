ce6eb12e558cd96a416115dc6feb616ee8069759
RomanFS76 <korvy85@gmail.com>
2024-05-30 13:22:25 +0300

    fix err

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,61 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import css from "./сomponents/SearchBox/SearchBox.module.css";
-import ContactForm from "./сomponents/ContactForm/ContactForm";
-import ContactList from "./сomponents/ContactList/ContactList";
-import SearchBox from "./сomponents/SearchBox/SearchBox";
-
+import css from "./components/SearchBox/SearchBox.module.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
 
 const usersData = [
-  {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-  {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-  {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-  {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
 ];
 
 function App() {
-  const [dataUser, setDataUser] = useState((dataUser=usersData)=>{
-    const localContact = localStorage.getItem('contact');
+  const [dataUser, setDataUser] = useState((dataUser = usersData) => {
+    const localContact = localStorage.getItem("contact");
     if (localContact) {
-      return  JSON.parse(localContact);
-      }
-      return dataUser;
-    });
+      return JSON.parse(localContact);
+    }
+    return dataUser;
+  });
 
-    useEffect(() => {
-      localStorage.setItem('contact', JSON.stringify(dataUser));
-    }, [dataUser]);
+  useEffect(() => {
+    localStorage.setItem("contact", JSON.stringify(dataUser));
+  }, [dataUser]);
 
+  const [filter, setFilter] = useState("");
 
-  const [filter, setFilter] = useState('');
+  const visibleUser = dataUser.filter((el) =>
+    el.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
-
-  const visibleUser = dataUser.filter(el => el.name.toLowerCase().includes(filter.toLowerCase()));
-  
   const addContact = (newContact) => {
-    setDataUser((prevContact)=>{
-      return [...prevContact,newContact]
-    })
+    setDataUser((prevContact) => {
+      return [...prevContact, newContact];
+    });
   };
 
-  const deleteContact =(contactId)=>{
-    setDataUser(prevContact=> {
-      return prevContact.filter(el => el.id !==contactId );
-    })
+  const deleteContact = (contactId) => {
+    setDataUser((prevContact) => {
+      return prevContact.filter((el) => el.id !== contactId);
+    });
   };
 
   return (
     <>
       <h1>Phonebook</h1>
       <ContactForm onAdd={addContact}></ContactForm>
-      <SearchBox className={css.searchBox} value={filter} onFilter={setFilter}></SearchBox>
-      <ContactList showUser={visibleUser} onDelete={deleteContact}></ContactList>
+      <SearchBox
+        className={css.searchBox}
+        value={filter}
+        onFilter={setFilter}
+      ></SearchBox>
+      <ContactList
+        showUser={visibleUser}
+        onDelete={deleteContact}
+      ></ContactList>
     </>
   );
 }
